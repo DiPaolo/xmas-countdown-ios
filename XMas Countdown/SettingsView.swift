348fbfc492eef251daaf57c93302f0875f32c6c9
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2020-11-22 11:05:06 +0700

    Remove CircleBox and add SettingsView

---
+++ XMas Countdown/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  XMas Countdown
+//
+//  Created by Pavel Ditenbir on 22.11.2020.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
