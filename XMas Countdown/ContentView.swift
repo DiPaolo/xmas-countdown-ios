0a16c0db9ffda70fd76dfffa4639692c7fe5e72a
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2020-11-22 11:06:19 +0700

    Add some playing around with UI stuff

--- XMas Countdown/ContentView.swift
+++ XMas Countdown/ContentView.swift
@@ -15,33 +15,83 @@ let dayX: Date = {
 struct ContentView: View {
     let daysLeft = Calendar.current.dateComponents([.day], from: Date(), to: dayX).day!
 
-    var textFont = Font.custom(
+    let textFont = Font.custom(
 //        "Noteworthy",
 //        "Papyrus",
         "Party LET",
         size: 70)
     
+    @State private var hidden = false
+    @State private var hasOffset = false
+    @State private var isRedBackground: Bool = false
+    @State private var toggleStatus = false
+
     var body: some View {
         VStack {
+            Button("Tap Me") {
+                self.hidden = true
+            }
+            .opacity(hidden ? 0 : 1)
+            .animation(.easeInOut(duration: 2))
+
+            Button("Tap Me") {
+                       withAnimation(.interpolatingSpring(
+                           mass: 1,
+                           stiffness: 80,
+                           damping: 4,
+                           initialVelocity: 0)) {
+                               self.hasOffset.toggle()
+                           }
+                   }
+                   .offset(y: hasOffset ? 40 : 0)
+
+            Toggle(isOn: $isRedBackground) {
+                Text("Red?")
+            }
+            .padding()
+
             Text("It's")
                 .font(textFont)
-            
-            Text("\(daysLeft)")
-                .font(.custom("Academy Engraved LET", size: 200))
-                .baselineOffset(-45)
-                .frame(width: 250, height: 250)
-                .foregroundColor(Color.white)
-                .background(Circle()
-                                .fill(Color.red)
-                                .frame(width: 250, height: 250)
-                )
-            
+
+            ZStack {
+                Text("\(daysLeft)")
+                    .font(.custom("Academy Engraved LET", size: 200))
+                    .baselineOffset(-45)
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(isRedBackground ? Color.white : Color.red)
+                    .background(Circle()
+                                    .fill(isRedBackground ? Color.red : Color.white)
+                                    .frame(width: 250, height: 250)
+                    )
+                    .opacity(toggleStatus ? 0.0 : 1.0)
+                    .onTapGesture {
+                        self.toggleStatus = !self.toggleStatus
+                        print("Show details for Enterprise")
+                    }
+
+                Text("Surprise")
+//                    .font(.custom("Academy Engraved LET", size: 200))
+//                    .baselineOffset(-45)
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(isRedBackground ? Color.red : Color.white)
+                    .background(Rectangle()
+                                    .fill(isRedBackground ? Color.white : Color.red)
+                                    .frame(width: 250, height: 250)
+                    )
+                    .opacity(!toggleStatus ? 0.0 : 1.0)
+                    .onTapGesture {
+                        self.toggleStatus = !self.toggleStatus
+                        print("Show details for Enterprise")
+                    }
+            }
+
             Text("DAYS")
                 .font(textFont)
                 .baselineOffset(-24)
             Text("before Christmas")
                 .font(textFont)
         }
+        
     }
 }
 
