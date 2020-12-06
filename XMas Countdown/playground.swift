9e771902e2ae9e61dbc1651ab6e81b03d3f68907
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2020-12-06 12:03:49 +0700

    Update Playground

--- XMas Countdown/playground.swift
+++ XMas Countdown/playground.swift
@@ -8,44 +8,13 @@
 import SwiftUI
 
 struct playground: View {
-    
-    @State var showingDetail = false
-
     var body: some View {
         Button(action: {
-            self.showingDetail.toggle()
+            UserNotificationManager.incrementBageCounter()
         }) {
-            Text("Info")
-        }.sheet(isPresented: $showingDetail) {
-//            DetailView()
-            VStack {
-                Text("WTF")
-                Spacer()
-                Button("Close") {
-                }
-            }
-            .padding()
+            Text("Increment app icon counter")
         }
     }
-    
-//    var body: some View {
-//        Image("The Star Wars/1")
-//            .sheet(isPresented: , content: {
-//                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Sheet Content")/*@END_MENU_TOKEN@*/
-//            }) {
-////            .popover(isPresented: .constant(true), arrowEdge: .bottom) {
-//                VStack {
-//                    Text("WTF")
-//                    Spacer()
-//                    Button("Close") {
-//                    }
-//                }
-//                .padding()
-//
-//            }
-////            .dismiss
-////            .resizable()
-//    }
 }
 
 struct playground_Previews: PreviewProvider {
